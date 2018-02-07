274d56c57a54af2281b2164102c7c4bbca06f112
micos7 <micos7@gmail.com>
2018-02-07 21:29:00 +0200

    Random food

--- app/src/main/java/ro/mihai/foodpicker/MainActivity.kt
+++ app/src/main/java/ro/mihai/foodpicker/MainActivity.kt
@@ -2,14 +2,29 @@ package ro.mihai.foodpicker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chines","Pizza","McDonlads","Pizza Hut")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
     }
 }
